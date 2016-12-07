59b7e59189a9806b9fb969b2148a5e5152a5ddfd
RByns <robin.baeyens.v4067@student.hogent.be>
2016-12-07 22:23:02 +0100

    Completed tutorial
    Restructuring of some files + bugs to fix

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+require('./models/Posts');
+require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-var mongoose = require('mongoose');
-require('./models/Posts');
-require('./models/Comments');
 
 mongoose.connect('mongodb://localhost/news');
 
@@ -27,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', index);
 app.use('/users', users);
